3f02c9f539197fbe0b123753b2d5e59a762f08ac
paramiwijesinghe <paramiwijesinghe05@gmail.com>
2024-10-07 20:15:55 +0700

    Create Navbar , Landing page

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import React, { Fragment } from 'react';
+import NavBar from './components/Navbar'
+import Landing from './components/Landing'
+import './App.css';
+
+const App = () => (  
+    <Fragment>
+      <NavBar/>
+      <Landing/>
+    </Fragment>
+  
+);
+export default App;
