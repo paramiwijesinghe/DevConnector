68021a7b83e7708289e4baaf967767107d4e91e9
paramiwijesinghe <paramiwijesinghe05@gmail.com>
2024-10-17 22:45:19 +0700

    Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
   GET_PROFILE,
-  PROFILE_ERROR,
-  CLEAR_PROFILE
+  PROFILE_ERROR
 } from './types';
 
 
@@ -21,4 +20,37 @@ export const getCurrentProfile = () => async dispatch => {
             payload: { msg: err.response.statusText, status: err.response.status }
         });
     }
+};
+
+//Create or update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        const res = await axios.post('/api/profile', formData, config);
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+        if(!edit){
+            history.push('/dashboard');
+        }
+    }
+    catch(err){
+        const errors = err.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });}
+
 }
