8f2034218363e4c2a4873328664b7daaafbfb456
paramiwijesinghe <paramiwijesinghe05@gmail.com>
2024-10-20 02:20:25 +0700

    Finish profile actions and reducer
    get all profiles , get repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
   ACCOUNT_DELETED,
+  GET_REPOS,
 } from './types';
 
 // Get the current user's profile
@@ -23,6 +25,60 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+//Get all profiles
+export const getProfiles = () => async dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get('/api/profile');
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+//Get all profiles by ID
+export const getProfilesById = () => async dispatch => {
+
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+
+//Get Github repos
+export const getGithubRepos = username => async dispatch => {
+
+
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
 
 // Create or update profile
 export const createProfile = (formData, navigate, edit = false) => async dispatch => {
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,10 +1,10 @@
 import {
   GET_PROFILE,
+  GET_PROFILES,
   PROFILE_ERROR,
   CLEAR_PROFILE,
   UPDATE_PROFILE,
-//   GET_PROFILES,
-//   GET_REPOS
+  GET_REPOS
 } from "../actions/types";
 
 
@@ -27,12 +27,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
-    // case GET_PROFILES:
-    //   return {
-    //     ...state,
-    //     profiles: payload,
-    //     loading: false
-    //   };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -46,12 +46,12 @@ export default function(state = initialState, action) {
         repos: [],
         loading: false
       };
-    // case GET_REPOS:
-    //   return {
-    //     ...state,
-    //     repos: payload,
-    //     loading: false
-    //   };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
+        loading: false
+      };
     default:
       return state;
   }
