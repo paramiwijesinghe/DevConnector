a4d271c8a932043321f4aaf69bd7cfa06b213c42
paramiwijesinghe <paramiwijesinghe05@gmail.com>
2024-10-22 01:28:59 +0700

    Add like and unlike functionality for a post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -17,3 +17,37 @@ export const getPosts = () => async (dispatch) => {
         });
     }
 };
+
+// Add post
+export const addLike = id => async (dispatch) => {
+    try {
+        const res = await axios.put(`/api/posts/like/${id}`); // Adjust the API endpoint
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data} // Assuming the API returns an array of posts
+        });
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status } // Handle errors
+        });
+    }
+};
+
+
+//Remove like
+export const removeLike = id => async (dispatch) => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${id}`); // Adjust the API endpoint
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { id, likes: res.data} // Assuming the API returns an array of posts
+        });
+    } catch (error) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status } // Handle errors
+        });
+    }
+};
+
